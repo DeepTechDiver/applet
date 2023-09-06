5dc235c8506dad91989372051212365dcec8b868
FengJiaqi <75030946+DeepTechDiver@users.noreply.github.com>
2023-09-06 19:08:44 +0800

    Update config.py

--- Pet-GPT/config.py
+++ Pet-GPT/config.py
@@ -1,5 +1,5 @@
 # [step 1]>> 例如： API_KEY = "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" （此key无效）
-API_KEY = "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"    # 可同时填写多个API-KEY，用英文逗号分割，例如API_KEY = "sk-openaikey1,sk-openaikey2,fkxxxx-api2dkey1,fkxxxx-api2dkey2"
+API_KEY = "sk-"    # 可同时填写多个API-KEY，用英文逗号分割，例如API_KEY = "sk-openaikey1,sk-openaikey2,fkxxxx-api2dkey1,fkxxxx-api2dkey2"
 
 # [step 2]>> 改为True应用代理，如果直接在海外服务器部署，此处不修改
 USE_PROXY = True
